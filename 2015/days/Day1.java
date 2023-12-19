4b09848930f751d3d40f4ca118ca81120678cddd
rzhou10 <rzhou10@gmail.com>
2023-12-19 16:49:57 -0500

    Day 1 2015

---
+++ 2015/days/Day1.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+class Day1 {
+    public static void main(String[] args) throws Exception {
+        File input = new File("../inputs/day1.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+        System.out.println(partOne(reader.readLine()));
+        System.out.println(partTwo(reader.readLine()));
+
+        reader.close();
+    }
+
+    static Integer partOne(String paranthesis) {
+        int counter = 0;
+
+        for (int i = 0; i < paranthesis.length(); i++) {
+            if (paranthesis.charAt(i) == ')') {
+                counter--;
+            } else {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    static Integer partTwo (String paranthesis) {
+        int index = 0;
+        int counter = 0;
+
+        for (int i = 0; i < paranthesis.length(); i++) {
+            if (counter == -1) {
+                index = i;
+                break;
+            }
+            if (paranthesis.charAt(i) == ')') {
+                counter--;
+            } else {
+                counter++;
+            }
+        }
+
+        return index;
+    }
+}

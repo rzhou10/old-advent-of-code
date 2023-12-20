aaa09d0be6a9700cfd877a46b43f1b0f0747ce17
rzhou10 <rzhou10@gmail.com>
2023-12-20 12:05:22 -0500

    Refactored duplicate code

--- 2015/days/Day3.java
+++ 2015/days/Day3.java
@@ -1,16 +1,14 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
 public class Day3 {
     public static void main(String[] args) throws Exception {
         File input = new File("../inputs/day3/day3.txt");
         int partOne = 0;
-        int partTwo = 0;
 
         BufferedReader reader = new BufferedReader(new FileReader(input));
 
-        partOne = partTwo(reader.readLine());
+        partOne = partOne(reader.readLine());
 
         System.out.println(partOne);
 
@@ -18,28 +16,9 @@ public class Day3 {
     }
 
     static Integer partOne(String directions) {
-        Integer x = 0;
-        Integer y = 0;
-        Set<String> houses = new HashSet<>();
-        houses.add(String.format("%s,%s", x, y));
+        Set<String> houses = calculatePath(directions);
 
-        for (int i = 0; i < directions.length(); i++) {
-            if (directions.charAt(i) == '>') {
-                x += 1;
-                houses.add(String.format("%s,%s", x, y));
-            } else if (directions.charAt(i) == '<') {
-                x -= 1;
-                houses.add(String.format("%s,%s", x, y));
-            } else if (directions.charAt(i) == '^') {
-                y += 1;
-                houses.add(String.format("%s,%s", x, y));
-            } else if (directions.charAt(i) == 'v') {
-                y -= 1;
-                houses.add(String.format("%s,%s", x, y));
-            }
-        }
-
-        return houses.size();
+        return houses.size() + 1;
     }
 
     static Integer partTwo(String directions) {
